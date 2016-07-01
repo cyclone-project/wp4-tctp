72bfba1144385f3c8e4ea1fbaa380d81162fc35a
LODYGENSKY Oleg <lodygens@lal.in2p3.fr>
2016-07-01 19:25:48 +0200

    tested as OpenStack postinstall script

--- src/bash/install-tctp.sh
+++ src/bash/install-tctp.sh
@@ -19,12 +19,22 @@ set -x
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
-sudo apt-get install -y g++ gcc git libncurses5-dev libssl-dev make openssl zlib1g-dev
+sudo apt-get install -y g++ gcc git libncurses5-dev libssl-dev make openssl zlib1g-dev bundler
+
+touch /tmp/wp4-tctp.log
+mkdir -p /home/ubuntu >> /tmp/wp4-tctp.log 2>&1
+cd /home/ubuntu >> /tmp/wp4-tctp.log 2>&1
 
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable --ruby=2.2.3
-. ${HOME}/.rvm/scripts/rvm
-. ${HOME}/.bashrc
+#. ${HOME}/.rvm/scripts/rvm
+. /usr/local/rvm/scripts/rvm
+touch /home/ubuntu/.bashrc
+echo ". /usr/local/rvm/scripts/rvm" >> /home/ubuntu/.bashrc
+echo "rvm install 2.3" >> /home/ubuntu/.bashrc
+
+echo "Downloading Tresor" >> /tmp/wp4-tctp.log 2>&1
+pwd >> /tmp/wp4-tctp.log 2>&1
 
 rm -Rf tresor-proxy
 git clone https://github.com/TU-Berlin-SNET/tresor-proxy.git
@@ -36,3 +46,6 @@ gem install bundler
 gem install json
 gem install i18n
 bundle install
+
+echo "Tresor successfully installed" >> /tmp/wp4-tctp.log 2>&1
+
